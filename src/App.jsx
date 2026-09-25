b938d6ebcda74cc8870c1ba5d07e79d3c72f6373
Rodrigonpp <rodrigonpp1@gmail.com>
2026-09-25 16:34:03 -0300

    Configurando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import "./App.css";
 //components
-import Counter from "./components/Counter";
-import Title from "./components/Title";
+import { Outlet } from "react-router-dom";
 //assets
-import NewYear from "./assets/new-year.jpg"
+import NewYear from "./assets/new-year.jpg";
 //hooks
-import useCountdown from "./hooks/useCountdown";
 
 function App() {
-  const x = useCountdown("Jan 1, 2027 00:00:00")
   return (
     <>
       <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
         <div className="container">
-          <Title title={"Contagem regressiva para 2027"} />
-          <div className="countdown-container">
-            <Counter title={"Dias"} number={2} />
-            <Counter title={"Horas"} number={2} />
-            <Counter title={"Minutos"} number={2} />
-            <Counter title={"Segundos"} number={2} />
-          </div>
+          <Outlet />
         </div>
       </div>
     </>
